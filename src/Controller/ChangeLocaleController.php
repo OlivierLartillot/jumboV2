1c4a8f834e10d1bc3fb3285ad6e16f5eada08216
Olivier Lartillot <olivier.lartillot@gmail.com>
2023-08-15 13:55:06 +0200

    trans effectué jusqu'au niveau du menu rep_briefing

--- src/Controller/ChangeLocaleController.php
+++ src/Controller/ChangeLocaleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChangeLocaleController extends AbstractController
 {
     #[Route('/change_locale/{locale}', name: 'app_change_locale')]
-    public function changeLocale($locale, Request $request): Response
+    public function changeLocale($locale, Request $request, EntityManagerInterface $em): Response
     {
+
+
+        $currentUser = $this->getUser();
+        $currentUser->setLanguage($locale);
+        $em->flush($currentUser);
+        
         // On stocke la langue dans la session
-        $request->getSession()->set('_locale', $locale);
+        $request->getSession()->set('_locale', $locale); 
 
         // On revient sur la page précédente
         return $this->redirect($request->headers->get('referer'));
