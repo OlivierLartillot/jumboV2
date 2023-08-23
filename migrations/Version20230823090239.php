284df836d5570c04315630725bc892721f267f56
Olivier Lartillot <olivier.lartillot@gmail.com>
2023-08-23 11:53:38 +0200

    fix class - création d une migration perso pour serveur en ligne

--- migrations/Version20230823090239.php
+++ migrations/Version20230823090239.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230819090239 extends AbstractMigration
+final class Version20230823090239 extends AbstractMigration
 {
     public function getDescription(): string
     {
