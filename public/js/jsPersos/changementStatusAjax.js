5a8a9b2c61d02dae364d85b77a19e805ea7325b6
OlivierLartillot <olivier.lartillot@gmail.com>
2023-12-07 09:48:25 +0100

    route pour mettre de la confidentiealité dans le test admin

--- public/js/jsPersos/changementStatusAjax.js
+++ public/js/jsPersos/changementStatusAjax.js
@@ -17,8 +17,6 @@ let app = {
     handleChange: function(event) {
         // cible le groupe de boutons sur lequel on a cliqué (ex: group-2)
         const targetGroup = event.target.dataset.group;
-        const serverUrl = document.getElementById('serverUrl');
-        console.log(serverUrl.textContent);
 
         const group = document.querySelectorAll('[data-group=' + targetGroup + ' ]');
         buttonsGroup = [];
@@ -27,8 +25,6 @@ let app = {
                 buttonsGroup.push(element);
             }
         });
-
-        console.log('--------------------------------------------')
 
         // dans la nodeList on a ces valeurs:
         const card = group[0];
@@ -44,15 +40,10 @@ let app = {
             alert.classList.remove('d-none')
         }
 
+        // Boite d'alerte, nouvelle valeure
         alert.getElementsByTagName('span')[1].textContent = event.target.textContent;
-       /*  document.getElementById('newStatus').textContent = event.target.textContent; */
 
         const transferArivalId = group[6].textContent;
-        const staffId = group[7].textContent
-
-
-        // Récupere les 2 inputs à cacher/montrer
-        /* console.log(event.target.id + ' ' + transferArivalId + ' ' + staffId) */
 
         let fetchOptions = {
             method: 'POST',
@@ -72,13 +63,11 @@ let app = {
             // => tu vires tous les btn-success ou btn-danger
             buttonsGroup.forEach(button => {           
                 if (button.id.toLowerCase() != data.toLowerCase()) {
-                    console.log(button)
                     button.classList.replace("btn-success", "btn-outline-success");
                     button.classList.replace("btn-danger", "btn-outline-danger");
                 }
                 //sinon tu vires  le btn-outline-danger or btn-outline-success
                 else {
-                    console.log(button)
                     button.classList.remove("btn-outline-success");
                     button.classList.add("btn-success");
                     button.classList.replace("btn-outline-danger", "btn-danger");
