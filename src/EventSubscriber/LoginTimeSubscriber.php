f61272dca9a85d874a3b42181a41de428c1bc006
OlivierLartillot <olivier.lartillot@gmail.com>
2024-01-02 11:24:49 +0100

    👌 IMPROVE: test deconnexion 3 secondes, redirection vers logout

--- src/EventSubscriber/LoginTimeSubscriber.php
+++ src/EventSubscriber/LoginTimeSubscriber.php
@@ -5,20 +5,24 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class LoginTimeSubscriber implements EventSubscriberInterface
 {
     private TokenStorageInterface $tokenStorage;
     private ContainerInterface $container;
+    private UrlGeneratorInterface $router;
 
-    public function __construct(TokenStorageInterface $tokenStorage, ContainerInterface $container)
+        public function __construct(TokenStorageInterface $tokenStorage, ContainerInterface $container, UrlGeneratorInterface $router)
     {
         $this->tokenStorage = $tokenStorage;
         $this->container = $container;
+        $this->router = $router;
     }
 
     public function onKernelRequest(RequestEvent $event)
@@ -30,7 +34,7 @@ class LoginTimeSubscriber implements EventSubscriberInterface
         if ($token && $token->getUser()) {
             $user = $token->getUser();
             // Vérifiez les rôles de l'utilisateur
-            if (in_array('ROLE_REP', $user->getRoles(), true)) {
+            if ((in_array('ROLE_REP', $user->getRoles(), true)) or (in_array('ROLE_AIRPORT', $user->getRoles(), true))) {
                 
                 // Vérifiez si le moment de la connexion a été défini
                 if (!$token->hasAttribute('_security_admin_login_time')) {
@@ -38,13 +42,22 @@ class LoginTimeSubscriber implements EventSubscriberInterface
                     $token->setAttribute('_security_admin_login_time', time());
                 } else {
                     // Si oui, vérifiez si la durée maximale est écoulée (30 secondes dans cet exemple)
-                    $maxDuration = 05; // Durée en secondes
+                    $maxDuration = 03; // Durée en secondes
                     $loginTime = $token->getAttribute('_security_admin_login_time');
                     $currentTime = time();
 
                     if (($currentTime - $loginTime) > $maxDuration) {
                         // Si la durée maximale est dépassée, déconnectez l'utilisateur
-                        $this->tokenStorage->setToken(null);
+                        //$this->tokenStorage->setToken(null);
+                        // Créez une réponse de redirection vers une URL spécifique
+                        $redirectUrl = $this->router->generate('app_logout');
+
+                        // Créez une réponse de redirection vers l'URL générée
+                        $response = new RedirectResponse($redirectUrl);
+
+                        // Assignez la réponse à l'événement
+                        $event->setResponse($response);
+                        
                     }
                 }
 
