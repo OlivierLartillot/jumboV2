58eff5fa86b22a15cb41a2d46a679c3efe899ec7
OlivierLartillot <olivier.lartillot@gmail.com>
2024-08-13 19:25:26 +0200

    👌 IMPROVE: amélioratopn de la suppression de l'espace apres le trait d'union du prénom (eg: jean- pierre) pour match avec la bdd, dans cette version on match les epaces avant et apres le trait d'union

--- src/Controller/TransferJoanController.php
+++ src/Controller/TransferJoanController.php
@@ -343,6 +343,8 @@ class TransferJoanController extends AbstractController
                 $nombre=str_replace("\n"," ",$nombre);
                 $nombre=str_replace("\r"," ",$nombre);
                 $nombre = preg_replace('/- {0,}/', '-', $nombre);
+                $nombre = preg_replace('/ {0,}-/', '-', $nombre);
+                $nombre = preg_replace('/ {0,}-  {0,}/', '-', $nombre);
             }
             if ($zonas != null) {
                 $zonas=str_replace("\n"," ",$zonas);
