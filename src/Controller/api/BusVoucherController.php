f5cf402b3c07230ffb18299d8bb6cae9b01674b3
OlivierLartillot <olivier.lartillot@gmail.com>
2024-10-21 18:58:12 +0200

    👌 IMPROVE: ajout des validations dans la requete + date dans le json

--- src/Controller/api/BusVoucherController.php
+++ src/Controller/api/BusVoucherController.php
@@ -5,6 +5,7 @@ namespace App\Controller\api;
 use App\Repository\CustomerCardRepository;
 use App\Repository\TransferArrivalRepository;
 use App\Repository\UserRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,23 +13,79 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BusVoucherController extends AbstractController
 {
-    // !!! modification du LoginTimeSubscriber pour permettre à cette route ublique de ne pas être
-    // renvoyée vers le login form
-    #[Route('/api/flights', name: 'app_api_public_flights_search', methods: ['GET'])]
-    public function bus(Request $request, UserRepository $userRepository, TransferArrivalRepository $transferArrivalRepository): Response
-    {
-        // l'utilisateur courant est un personnel d'aéroport
-        $currentUserAirport = $this->getUser()->getAirport();
-        // TODO:  si l'utilisateur n'a pas associé d'aéroport, tu renvoie une erreur lisible
 
-        $date = $request->query->get("date");
-        // TODO: rechercher tous les vols de ce jour pour cet aéroport
-    
+    // 1. Controlleurs de l'api Bus Voucher
+    // 2. Les méthodes propres à cette classe
+
+
+    //***********************************************************************************************/ 
+    //************************************ 1 . Controlleurs *****************************************/
+    //***********************************************************************************************/ 
+
+    /**
+     * Objectif: Route qui permet de récupérer tous les vols de ce jour pour l'aéroport favori du personnel connecté
+     * Params: date
+     */
+    #[Route('/api/flights', name: 'app_api_public_flights_search', methods: ['GET'])]
+    public function flights(Request $request, TransferArrivalRepository $transferArrivalRepository): Response
+    {
+        // l'utilisateur courant est un personnel d'aéroport, on va chercher son aéroport associé
+        $currentUserAirport = $this->getUser()->getAirport();
+        
+        // recevoir la date en query string 
+        $date = $request->query->get('date');
+        
+        // Gestion des ERREURS
+        // si l'utilisateur n'a pas associé d'aéroport, tu renvoie une erreur lisible
+        if ($currentUserAirport == null) { return $this->json("You must choose a favorite Airport in your settings");}
+        // si il n'y a pas de date fournie
+        if (!isset($date)) {return $this->json("You must choose a date");}
+        $checkDate = $this->valideDate($date);
+        if (!$checkDate) { return $this->json(" ");}
+        
+        // rechercher tous les vols de ce jour pour cet aéroport
+        $flightNumbers = $transferArrivalRepository->searchFlightsNumberForThisAeroportrepAndDay($currentUserAirport, $date);
+
         return $this->json(
-            $transferArrivalRepository->searchFlightsNumberForThisAeroportrepAndDay($currentUserAirport, $date),
+            [$flightNumbers, $date],
             200,
             [],
-            ['groups' => 'api_public_test']   
         );
+    }
+
+
+    /**
+     * Objectif: Route qui permet de récupérer la liste des passagers en fonction des vols choisis et du jour pour ce personnel connecté
+     * Params: date, flightNumberList
+     */
+    // 
+    #[Route('/api/voucher', name: 'app_api_voucher_list', methods: ['GET'])]
+    public function voucherPerDayAndFlight(Request $request,TransferArrivalRepository $transferArrivalRepository): Response
+    {
+    
+        
+
+
+        return $this->json('la répponse');
+    }
+
+
+
+
+
+    //***********************************************************************************************/ 
+    //******************************  2. METHODES propres à l'objet *********************************/
+    //***********************************************************************************************/ 
+
+    /**
+     * Vérifie la validité de la date, fixée au format Y-m-d
+     * @param mixed $date
+     * @param mixed $format
+     * @return bool
+     */
+    function valideDate($date, $format = 'Y-m-d'):bool
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
     }
 }
